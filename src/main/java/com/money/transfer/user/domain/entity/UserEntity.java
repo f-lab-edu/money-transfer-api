2a3adc1114953ac8f825c5b2843676880ee3c6e6
wugatama <dnr7410@naver.com>
2025-10-13 15:24:31 +0900

    feat: UserEntity setter 추가

--- src/main/java/com/money/transfer/user/domain/entity/UserEntity.java
+++ src/main/java/com/money/transfer/user/domain/entity/UserEntity.java
@@ -10,12 +10,14 @@ import java.time.LocalDateTime;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Entity
 @Getter
+@Setter
 @Table(name = "\"user\"")
 @EntityListeners(AuditingEntityListener.class)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
